e6740d853d4c6c566420141a443230444b8ce07d
Oksana2020LA <144433260+Oksana2020LA@users.noreply.github.com>
2023-11-28 00:20:43 -0800

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -13,10 +13,6 @@ const newNumbers = [];
   return newNumbers;
 }
 
-const randomNumbers = [8, 15, 30, 42, 55];
-const randomValue = Math.floor(Math.random() * 58); 
-
-
 //audit
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
@@ -24,5 +20,3 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-console.log(filterArray(randomNumbers, randomValue));
